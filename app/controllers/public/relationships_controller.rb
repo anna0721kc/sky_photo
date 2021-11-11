0450e65b1c661e9a2385eb0348354903f337d837
anna0721kc <pipi0721s2@gmail.com>
2021-11-11 13:01:59 +0000

    フォローリストレイアウト

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -3,14 +3,14 @@ class Public::RelationshipsController < ApplicationController
   def create
     follow = current_customer.active_relationships.build(follower_id: params[:customer_id])
     follow.save
-    redirect_to customer_path(params[:customer_id])
+    redirect_to follows_customer_path(current_customer)
   end
 
 
   def destroy
     follow = current_customer.active_relationships.find_by(follower_id: params[:customer_id])
     follow.destroy
-    redirect_to customer_path(params[:customer_id])
+    redirect_to followers_customer_path(current_customer)
   end
 
 end
