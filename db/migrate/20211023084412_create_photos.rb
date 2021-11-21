d038cce007a50e8307715c40be2b5a02c6871aed
anna0721kc <pipi0721s2@gmail.com>
2021-11-21 06:05:04 +0000

    rds向けにtext型にdefault値をセット

--- db/migrate/20211023084412_create_photos.rb
+++ db/migrate/20211023084412_create_photos.rb
@@ -6,7 +6,7 @@ class CreatePhotos < ActiveRecord::Migration[5.2]
       t.string :address
       t.float :latitude
       t.float :longitude
-      t.text :introduction,   null: false, default: ""
+      t.text :introduction,   null: false, default: "未入力"
       t.timestamps
     end
   end
