b158d0a563c8c536dc9e9dd25e10d3a543e770a0
anna0721kc <pipi0721s2@gmail.com>
2021-11-21 05:53:48 +0000

    rds向けにmigrateを編集

--- db/migrate/20211023084412_create_photos.rb
+++ db/migrate/20211023084412_create_photos.rb
@@ -1,12 +1,12 @@
 class CreatePhotos < ActiveRecord::Migration[5.2]
   def change
     create_table :photos do |t|
-      t.integer :customer_id,              null: false, default: ""
-      t.string :image_id,              null: false, default: ""
-      t.string :address,              null: false, default: ""
-      t.float :latitude,              null: false, default: ""
-      t.float :longitude,              null: false, default: ""
-      t.text :introduction,              null: false, default: ""
+      t.integer :customer_id
+      t.string :image_id
+      t.string :address
+      t.float :latitude
+      t.float :longitude
+      t.text :introduction,   null: false, default: ""
       t.timestamps
     end
   end
--- db/migrate/20211023084435_create_favorites.rb
+++ db/migrate/20211023084435_create_favorites.rb
@@ -1,8 +1,8 @@
 class CreateFavorites < ActiveRecord::Migration[5.2]
   def change
     create_table :favorites do |t|
-      t.integer :customer_id,              null: false, default: ""
-      t.integer :photo_id,              null: false, default: ""
+      t.integer :customer_id
+      t.integer :photo_id
       t.timestamps
     end
   end
