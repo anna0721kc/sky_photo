9333357195f36d1ef55a9aadc907d700b826f775
anna0721kc <pipi0721s2@gmail.com>
2021-11-29 11:26:51 +0000

    photos/showページ「no_image.jpg」画像をassetファイルに追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -10,16 +10,16 @@ class Customer < ApplicationRecord
 
  # フォローする側から見て、フォローされる側を(中間テーブルを経由して)集める。なので親はfollowing_id(フォローする側)
   has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
- # 中間テーブルを経由して「follower」モデルのCustomer(フォローされた側)を集めることを「followings」と定義
+ # 中間テーブルを経由して「follower」モデルのCustomer(フォローされた側)を集めることを「followings」と定義。sourceでモデルの参照元を指定／throughオプションで中間テーブルを指定
   has_many :followings, through: :active_relationships, source: :follower
 
  # フォローされる側から見て、フォローしてくる側を(中間テーブルを経由して)集める。なので親はfollower_id(フォローされる側)
   has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
- # 中間テーブルを経由して「following」モデルのCustomer(フォローする側)を集めることを「followers」と定義
+ # 中間テーブルを経由して「following」モデルのCustomer(フォローする側)を集めることを「followers」と定義。sourceでモデルの参照元を指定／throughオプションで中間テーブルを指定
   has_many :followers, through: :passive_relationships, source: :following
 
   def followed_by?(customer)
- # follow済みかどうか判定(今自分(引数のcustomer)がフォローしようとしているユーザー(レシーバー)がフォローされているユーザー(つまりpassive)の中から、引数に渡されたユーザー(自分)がいるかどうかを調べる)
+ # follow済みかどうか判定(今自分(引数のcustomer)がフォローしようとしているユーザーが、フォローされているユーザー(passive)の中から、引数に渡されたユーザー(自分)がいるかどうかを調べる)
     passive_relationships.find_by(following_id: customer.id).present?
   end
 
